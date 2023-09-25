aaca5b8cf1605977a66e500e4925932568ad85f4
DELL <lamyaechouinna1@gmail.com>
2023-09-25 18:07:51 +0100

    by mounir

--- models/models.py
+++ models/models.py
@@ -1,9 +1,11 @@
 from transformers import pipeline
+import os
 audio = pipeline("automatic-speech-recognition", model="openai/whisper-small")
 
 
 def AUDIO(audio_path):
     text = audio(audio_path)
+    os.remove(audio_path)
     return text
 
 
